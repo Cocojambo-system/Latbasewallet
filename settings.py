b958eb1e3c903d8b16b980415f830cd9e7e27bf0
Cocojambo-system <wudmytrotest200@gmail.com>
2024-10-01 18:12:16 +0200

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,6 @@
+#Requirements
+
+# 1. Python 3.6+: Ensure that Python is installed on your system.
+# 2. Install web3.py: pip install web3
+# 3. Base Network RPC URL: You will need an RPC URL to connect to the Base network. You can use public RPC endpoints or set up your own node.
+
